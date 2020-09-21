fb2c5211659aad32b09c2c62b4b288ff43e18688
HoaNguyen <hoa.nguyen2@asiantech.vn>
2020-09-21 16:55:28 +0700

    Config Hierarchy

---
+++ FinalProject/View/Controllers/0. Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  FinalProject
+//
+//  Created by NXH on 9/21/20.
+//  Copyright © 2020 MBA0176. All rights reserved.
+//
+
+import UIKit
+
+final class LoginViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
