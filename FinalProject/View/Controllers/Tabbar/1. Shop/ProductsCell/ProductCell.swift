bbec137fa2b85b3081db433fa1146370cc8702be
HoaNguyen <hoa.nguyen2@asiantech.vn>
2020-09-24 17:44:00 +0700

    change UI cell

--- FinalProject/View/Controllers/Tabbar/1. Shop/ProductsCell/ProductCell.swift
+++ FinalProject/View/Controllers/Tabbar/1. Shop/ProductsCell/ProductCell.swift
@@ -23,9 +23,15 @@ final class ProductCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        configCell()
     }
 
 // MARK: - Private Functions
+    private func configCell() {
+        self.layer.cornerRadius = 10
+        self.layer.masksToBounds = true
+    }
+    
     private func updateView() {
         guard let product = viewModel?.product else {
             return
