8f3b60263b740b04f16ba59058c268d0c15b8be2
HoaNguyen <hoa.nguyen2@asiantech.vn>
2020-09-24 10:33:41 +0700

    Implement UI Shop

---
+++ FinalProject/Model/Schema/Product.swift
@@ -0,0 +1,14 @@
+//
+//  Product.swift
+//  FinalProject
+//
+//  Created by NXH on 9/24/20.
+//  Copyright © 2020 MBA0176. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    var image: String
+    var description: String
+}
